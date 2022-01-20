c9e6c33cdbd9ef964c73a639f57f27bd6563dc27
raito-kitada <>
2022-01-20 15:48:02 +0900

    first commit

---
+++ 卒業論文/img5/png/GD/change.sh
@@ -0,0 +1,21 @@
+mv undx-DTLZ2 undx-DTLZ2.png
+mv undx-DTLZ3 undx-DTLZ3.png
+mv undx-DTLZ4 undx-DTLZ4.png
+mv undx-WFG1 undx-WFG1.png
+mv undx-WFG2 undx-WFG2.png
+mv undx-WFG6 undx-WFG6.png
+mv undx-WFG8 undx-WFG8.png
+mv undx-WFG9 undx-WFG9.png
+mv undx-ZDT1 undx-ZDT1.png
+mv undx-ZDT4 undx-ZDT4.png
+
+
+
+
+
+
+
+
+
+
+
---
+++ 卒業論文/img5/png/HV/change.sh
@@ -0,0 +1,21 @@
+mv de-DTLZ2 de-DTLZ2.png
+mv de-DTLZ3 de-DTLZ3.png
+mv de-DTLZ4 de-DTLZ4.png
+mv de-WFG1 de-WFG1.png
+mv de-WFG2 de-WFG2.png
+mv de-WFG6 de-WFG6.png
+mv de-WFG8 de-WFG8.png
+mv de-WFG9 de-WFG9.png
+mv de-ZDT1 de-ZDT1.png
+mv de-ZDT4 de-ZDT4.png
+
+
+
+
+
+
+
+
+
+
+
